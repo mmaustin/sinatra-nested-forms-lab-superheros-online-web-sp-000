88411e0fcc6c73d1ade3247c6dc228ca516a348b
McCray <tubach75@hotmail.com>
2021-02-20 11:49:39 -0500

    Done.

--- spec/super_sinatra_forms_spec.rb
+++ spec/super_sinatra_forms_spec.rb
@@ -8,7 +8,7 @@ describe App do
     it 'sends a 200 status code' do
       expect(last_response.status).to eq(200)
     end
-
+    
     it 'renders super hero form' do 
       expect(last_response.body).to include("Create a Team and Heroes!")
       expect(last_response.body).to include("<form")
